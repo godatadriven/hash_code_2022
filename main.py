bec0f5817e7cdc3100d9c27b1dcf16f48a99073b
Jelte Hoekstra <jeltehoekstra@godatadriven.com>
2022-02-24 19:33:06 +0100

    Main loop, project_value

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import os
 import sys
 
 Contributor = namedtuple("Contributor", field_names=["name", "skills"])
-Project = namedtuple("Project", field_names=["name", "days", "score", "best_before", "roles"])
+Project = namedtuple(
+    "Project", field_names=["name", "days", "score", "best_before", "roles"]
+)
 Skill = namedtuple("Skill", field_names=["name", "level"])
 Role = namedtuple("Role", field_names=["name", "level"])
 
+
 def parse_file(filename):
-    fp = open(filename, 'r')
+    fp = open(filename, "r")
     lines = fp.readlines()
     c, p = map(int, lines.pop(0).split())
 
@@ -21,9 +24,9 @@ def parse_file(filename):
         for j in range(int(nr_skills)):
             name_of_skill, level = lines.pop(0).split()
             skills.append(Skill(name_of_skill, int(level)))
-        
+
         contributors.append(Contributor(name, skills))
-    
+
     projects = []
     for i in range(p):
         name, days, score, best_before, nr_roles = lines.pop(0).split()
@@ -32,8 +35,27 @@ def parse_file(filename):
             name_of_skill, level = lines.pop(0).split()
             roles.append(Role(name_of_skill, int(level)))
 
-        projects.append(Project(name, int(days), int(score), int(best_before), roles))
+        projects.append(
+            Project(name, int(days), int(score), int(best_before), roles)
+        )
 
     return contributors, projects
 
-print(parse_file(sys.argv[1]))
+
+def project_value(project: Project, timestep: int) -> float:
+    """Remaining project score over total project days"""
+    discount = max(0, timestep - project.best_before)
+    return (project.score - discount) / len(project.roles) * project.days
+
+
+PROBLEMS = [
+    "a_an_example.in.txt",
+    "b_better_start_small.in.txt",
+    "c_collaboration.in.txt",
+    "d_dense_schedule.in.txt",
+    "e_exceptional_skills.in.txt",
+    "f_find_great_mentors.in.txt",
+]
+for problem_filename in PROBLEMS:
+    contributors, projects = parse_file(problem_filename)
+    # project_values = [project_value(p, 0) for p in projects]
